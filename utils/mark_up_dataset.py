1deac3d43b4a43057f42acf8e019366971960feb
Bogdan <bogdan.allaberdin2@gmail.com>
2025-05-25 19:30:45 +0300

    [fix] формат лейблов реального датасета как у YOLO

--- utils/mark_up_dataset.py
+++ utils/mark_up_dataset.py
@@ -12,21 +12,23 @@ def find_bounding_boxes(image_path):
     bboxes = []
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        bboxes.append((x + w / 2, y + h / 2, w, h))
+        bboxes.append((0, (x + w / 2) / WIDTH, (y + h / 2) / HEIGHT, w / WIDTH, h / HEIGHT)) 
     return bboxes
 
 if __name__ == '__main__':
-	input_dirs = ('datasets/BCCD_Dataset_with_mask/train', 
-				  'datasets/BCCD_Dataset_with_mask/test')
-	for in_dir in input_dirs:
-		os.makedirs(Path(in_dir) / 'bboxes', exist_ok=True)
+    input_dirs = ('datasets/BCCD_Dataset_with_mask/train', 
+                  'datasets/BCCD_Dataset_with_mask/test')
+    WIDTH = 1944
+    HEIGHT = 1383
+    for in_dir in input_dirs:
+        os.makedirs(Path(in_dir) / 'labels', exist_ok=True)
 
-		in_folder = Path(in_dir) / 'mask'
-		out_folder = Path(in_dir) / 'bboxes'
+        in_folder = Path(in_dir) / 'mask'
+        out_folder = Path(in_dir) / 'labels'
 
-		for file in in_folder.iterdir():
-			bboxes = find_bounding_boxes(str(file))
-			with open(str(out_folder / f'{file.stem}.txt'), 'w') as f:
-				for bbox in bboxes:
-					f.write(' '.join(map(str, bbox)) + '\n')
+        for file in in_folder.iterdir():
+            bboxes = find_bounding_boxes(str(file))
+            with open(str(out_folder / f'{file.stem}.txt'), 'w') as f:
+                for bbox in bboxes:
+                    f.write(' '.join(map(str, bbox)) + '\n')
 
